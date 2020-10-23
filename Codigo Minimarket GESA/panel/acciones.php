3eed8b9626af9cc7ef9464caed5b8c49f8fb16fb
Josma17 <71160988+Josma17@users.noreply.github.com>
2020-10-23 11:37:48 -0500

    Actualización código y documento

---
+++ Codigo Minimarket GESA/panel/acciones.php
@@ -0,0 +1,102 @@
+<?php
+require '../vendor/autoload.php';
+
+$productos = new Minimarket\Productos;
+
+if($_SERVER['REQUEST_METHOD'] ==='POST'){
+
+    if ($_POST['limpieza']==='Registrar'){
+
+        if(empty($_POST['titulo']))
+            exit('Completar titulo');
+        
+        if(empty($_POST['descripcion']))
+            exit('Completa la descripción');
+
+        if(empty($_POST['categoria_ID']))
+            exit('Seleccionar una Categoria');
+
+        if(!is_numeric($_POST['categoria_ID']))
+            exit('Seleccionar una Categoria válida');
+
+        
+         $_params = array(
+            'titulo'=>$_POST['titulo'],
+            'descripcion'=>$_POST['descripcion'],
+            'foto'=> subirFoto(),
+            'precio'=>$_POST['precio'],
+            'categoria_ID'=>$_POST['categoria_ID'],
+            'fecha'=> date('Y-m-d')
+        );
+        $rpt = $productos->registrar($_params);
+         
+        if($rpt)
+            header("Location: productos/index.php");
+        else
+            print "Error al reguistrar una producto";
+        
+    }
+    if ($_POST['limpieza']==='Actualizar'){
+
+        if(empty($_POST['titulo']))
+        exit('Completar titulo');
+    
+    if(empty($_POST['descripcion']))
+        exit('Completar titulo');
+
+    if(empty($_POST['categoria_ID']))
+        exit('Seleccionar una Categoria');
+
+    if(!is_numeric($_POST['categoria_ID']))
+        exit('Seleccionar una Categoria válida');
+
+    
+    $_params = array(
+        'titulo'=>$_POST['titulo'],
+        'descripcion'=>$_POST['descripcion'],
+        'precio'=>$_POST['precio'],
+        'categoria_ID'=>$_POST['categoria_ID'],
+        'fecha'=> date('Y-m-d'),
+        'ID'=>$_POST['ID'],
+    );
+
+    if(!empty($_POST['foto_temp']))
+        $_params['foto'] = $_POST['foto_temp'];
+    
+    if(!empty($_FILES['foto']['name']))
+        $_params['foto'] = subirFoto();
+
+
+    $rpt = $productos->actualizar($_params);
+    if($rpt)
+        header('Location: productos/index.php');
+    else
+        print 'Error al actualizar una productos';
+    }
+}
+if($_SERVER['REQUEST_METHOD'] ==='GET'){
+
+    $ID = $_GET['ID'];
+
+    $rpt = $productos->eliminar($ID);
+    
+    if($rpt)
+        header('Location: productos/index.php');
+    else
+        print 'Error al eliminar un producto';
+
+
+}
+
+
+function subirFoto() {
+
+    $carpeta = __DIR__.'/../upload/';
+
+    $archivo = $carpeta.$_FILES['foto']['name'];
+
+    move_uploaded_file($_FILES['foto']['tmp_name'],$archivo);
+
+    return $_FILES['foto']['name'];
+
+}
---
+++ Codigo Minimarket GESA/src/Productos.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Minimarket;
+
+class Productos{
+
+    private $config;
+    private $cn = null;
+
+    public function __construct(){
+
+        $this->config = parse_ini_file(__DIR__.'/../config.ini') ;
+
+        $this->cn = new \PDO( $this->config['dns'], $this->config['usuario'],$this->config['clave'],array(
+            \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+        ));
+        
+    }
+    public function registrar($_params){
+        $sql = "INSERT INTO `productos`(`titulo`, `descripcion`, `foto`, `precio`, `categoria_ID`, `fecha`) 
+        VALUES (:titulo,:descripcion,:foto,:precio,:categoria_ID,:fecha)";
+
+        $resultado = $this->cn->prepare($sql);
+        $_array = array(
+            ":titulo" => $_params['titulo'],
+            ":descripcion" => $_params['descripcion'],
+            ":foto" => $_params['foto'],
+            ":precio" => $_params['precio'],
+            ":categoria_ID" => $_params['categoria_ID'],
+            ":fecha" => $_params['fecha'],
+        );
+
+        if($resultado->execute($_array))
+            return true;
+
+        return false;
+    }
+
+    public function actualizar($_params){
+        $sql = "UPDATE `productos` SET `titulo`=:titulo,`descripcion`=:descripcion,`foto`=:foto,
+        `precio`=:precio,`categoria_ID`=:categoria_ID,`fecha`=:fecha  WHERE `ID`=:ID";
+
+        $resultado = $this->cn->prepare($sql);
+
+        $_array = array(
+            ":titulo" => $_params['titulo'],
+            ":descripcion" => $_params['descripcion'],
+            ":foto" => $_params['foto'],
+            ":precio" => $_params['precio'],
+            ":categoria_ID" => $_params['categoria_ID'],
+            ":fecha" => $_params['fecha'],
+            ":ID" =>  $_params['ID']
+        );
+
+        if($resultado->execute($_array))
+            return true;
+
+        return false;
+    }
+
+    public function eliminar($ID){
+        $sql = "DELETE FROM `productos` WHERE `ID`=:ID";
+
+        $resultado = $this->cn->prepare($sql);
+        
+        $_array = array(
+            
+            ":ID" =>  $ID
+        );
+
+        if($resultado->execute($_array))
+            return true;
+
+        return false;
+    }
+
+    public function mostrar(){
+        $sql = "SELECT productos.ID, titulo, descripcion,foto,nombre,precio,fecha,estado FROM productos 
+        
+        INNER JOIN categorias
+        ON productos.categoria_ID = categorias.ID ORDER BY productos.ID DESC
+        ";
+        
+        $resultado = $this->cn->prepare($sql);
+
+        if($resultado->execute())
+            return $resultado->fetchAll();
+
+        return false;
+    }
+
+    public function mostrarPorID($ID){
+        
+        $sql = "SELECT * FROM `productos` WHERE `ID`=:ID ";
+        
+        $resultado = $this->cn->prepare($sql);
+        $_array = array(
+
+            ":ID" => $ID
+        );
+
+        if($resultado->execute($_array))
+            return $resultado->fetch();
+
+        return false;
+    }
+
+
+    
+}
+
+
+
